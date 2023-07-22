f050901894f887c3635326c24bc0923d02fe52df
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2023-07-22 18:42:12 +0100

    Explicitly export types && Changed name of compiled files to match project name

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "SolidHelperVue",
-      fileName: "index",
+      fileName: "solid-helper-vue",
     },
     rollupOptions: {
       external: ["vue", "pinia", "@inrupt/solid-client-authn-browser"],
